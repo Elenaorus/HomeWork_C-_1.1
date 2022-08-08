93c0fac87720593131a5d679b553a47236f72f3c
Elena <elenaoruskoeva@yandex.ru>
2022-08-08 20:50:20 +0700

    Example001

---
+++ Example/Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ввeдите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввeдите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b) Console.WriteLine("Первое число больше второго");
+if (b>a) Console.WriteLine("Второе число больше первого");
+if (b==a) Console.WriteLine("Эти числа равны");
