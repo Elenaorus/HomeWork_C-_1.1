d2b9657c8161f837a9df4ffca19cc7dcf3e80fc2
Elena <elenaoruskoeva@yandex.ru>
2022-08-08 21:01:06 +0700

    Example002

---
+++ Example/Example002/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int z = Convert.ToInt32(Console.ReadLine());
+if (a>b) 
+{
+    if (a>z)
+    {
+        Console.WriteLine("Максимальное число = " + a);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное число = " + z); 
+    }
+}
+if (b>a) 
+{
+    if (b>z)
+    {
+        Console.WriteLine("Максимальное число = " + b);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное число = " + z); 
+    }
+}
